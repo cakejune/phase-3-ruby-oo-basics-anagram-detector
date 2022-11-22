f297a1e0a9a743e5d1c5c35e8fab400248451bde
cakejune <tyq444@gmail.com>
2022-11-22 15:07:16 -0500

    commit tree d7ca36e95352b8f5d877c5e31bd30a905b275ca0

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,39 @@
+require 'pry'
 # Your code goes here!
+class Anagram
+
+def initialize word
+@word=word
+end
+
+
+def word=(word)
+    @word=word
+end
+
+def word
+    @word
+end
+
+def match word_array
+word_array.split.map do |word|
+letter_array = []
+letter_array = word.chars.map{|letter| letter}
+end
+end
+
+def matchv2 string
+    ordered_string = string.chars.sort
+    return [] unless ordered_string == @word.chars.sort
+    return string
+end
+
+def array_match array
+    array.map{|string| matchv2(string)}
+end
+
+
+end
+
+new_anagram = Anagram.new("test")
+binding.pry
